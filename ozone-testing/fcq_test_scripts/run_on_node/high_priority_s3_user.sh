b6c1668f52ec8131f86d121e3ee7ad84c9ed70f4
Christos Bisias <christosbis@gmail.com>
2023-09-21 21:33:43 +0300

    10 req per 1 min for low prio

--- ozone-testing/fcq_test_scripts/run_on_node/high_priority_s3_user.sh
+++ ozone-testing/fcq_test_scripts/run_on_node/high_priority_s3_user.sh
@@ -16,7 +16,7 @@ export AWS_ACCESS_KEY=$oz_user AWS_SECRET_KEY=pass
 
 for i in {0..99} 
 do
-  sleep 2
+  sleep 60
   /hadoop/app/ozone/bin/ozone freon s3kg -e http://$S3G1_HOSTNAME:9878 -t 3 -n 10 -b bucket1 -p s3high$i
 done
 
